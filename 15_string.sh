d9ae7660f84e29151a3a320431b09d5478a7908d
artemdashkov <artemdashkov@gmail.com>
2023-08-08 17:29:00 +0300

    add all files

--- 15_string.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-c=1
-touch tf_4.txt
-while [ $c != 15 ]
-do
-	cat >> tf_4.txt line № $c
-	(($c++))
-	cat tf_4
-done
---
+++ dir_1/inner_dir_1/15_string.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+touch tf_4.txt
+for ((counter= 1; counter <= 15; counter++))
+do
+echo "Number is $counter"
+done > tf_4.txt
